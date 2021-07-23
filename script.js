baa3be1a1b5459e256fb1ff1c1b601ebb8fbf1b3
Mansour <mm71990@gmail.com>
2021-07-24 02:21:58 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+/* script for toggle menu*/
+var navlinks = document.getElementById("navlinks");
+var showMenu;
+var hideMenu;
+
+function showMenu(){
+    navlinks.style.right = "0";
+}
+function hideMenu(){
+    navlinks.style.right = "-200px";
+}/* script for toggle menu end here */
+
